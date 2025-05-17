a01d18b9320436eebf30bdc06f5cfa5727145c5d
alison <adevpr11@gmail.com>
2025-05-17 19:00:11 -0300

    Adiciona lógica para instanciar objetos das classes FuncionarioController e FuncionarioRepository

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,10 +1,14 @@
 package principal;
 
+import controllers.FuncionarioController;
+import repositories.FuncionarioRepositoryJSON;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		var funcionarioRepository = new FuncionarioRepositoryJSON();
+		var funcionarioController = new FuncionarioController(funcionarioRepository);
+		funcionarioController.exibirOpcoes();
 	}
 
 }
