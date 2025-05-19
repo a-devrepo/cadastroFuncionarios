2564f75d925108c534fea8a39fa52e1be96d9b1d
alison <adevpr11@gmail.com>
2025-05-19 19:36:00 -0300

    Adiciona regras de validação a classe Funcao

--- src/entities/Funcao.java
+++ src/entities/Funcao.java
@@ -1,17 +1,21 @@
 package entities;
 
+import java.util.Objects;
 import java.util.UUID;
+
+import exceptions.DomainException;
 
 public class Funcao {
 
 	private UUID id;
 	private String nome;
-	
-	public Funcao() {}
-	
+
+	public Funcao() {
+	}
+
 	public Funcao(UUID id, String nome) {
-		this.id = id;
-		this.nome = nome;
+		this.setId(id);
+		this.setNome(nome);
 	}
 
 	public UUID getId() {
@@ -19,6 +23,9 @@ public class Funcao {
 	}
 
 	public void setId(UUID id) {
+		if (Objects.isNull(id)) {
+			throw new DomainException("ID não pode ser nulo.");
+		}
 		this.id = id;
 	}
 
@@ -27,6 +34,9 @@ public class Funcao {
 	}
 
 	public void setNome(String nome) {
+		if (nome == null || nome.isBlank()) {
+			throw new DomainException("Nome não pode ser nulo ou vazio.");
+		}
 		this.nome = nome;
 	}
 
