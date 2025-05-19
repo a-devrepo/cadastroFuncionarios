41a96206ec07bf1d0fc86f7f446e7fbf0a46bbcb
alison <adevpr11@gmail.com>
2025-05-19 19:31:28 -0300

    Adiciona regras de validação a classe Funcionario

--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -2,6 +2,8 @@ package entities;
 
 import java.util.UUID;
 
+import exceptions.DomainException;
+
 public class Funcionario extends Pessoa {
 
 	private String cpf;
@@ -9,16 +11,17 @@ public class Funcionario extends Pessoa {
 	private Double salario;
 	private Setor setor;
 	private Funcao funcao;
-	
-	public Funcionario() {}
+
+	public Funcionario() {
+	}
 
 	public Funcionario(UUID id, String nome, String cpf, String matricula, Double salario, Setor setor, Funcao funcao) {
 		super(id, nome);
-		this.cpf = cpf;
-		this.matricula = matricula;
-		this.salario = salario;
-		this.setor = setor;
-		this.funcao = funcao;
+		this.setCpf(cpf);
+		this.setMatricula(matricula);
+		this.setSalario(salario);
+		this.setSetor(setor);
+		this.setFuncao(funcao);
 	}
 
 	public String getCpf() {
@@ -26,6 +29,9 @@ public class Funcionario extends Pessoa {
 	}
 
 	public void setCpf(String cpf) {
+		if (cpf == null || cpf.isBlank()) {
+			throw new DomainException("CPF não pode ser nulo ou vazio");
+		}
 		this.cpf = cpf;
 	}
 
@@ -34,6 +40,9 @@ public class Funcionario extends Pessoa {
 	}
 
 	public void setMatricula(String matricula) {
+		if (matricula == null || matricula.isBlank()) {
+			throw new DomainException("Matrícula não pode ser nulo ou vazio");
+		}
 		this.matricula = matricula;
 	}
 
@@ -42,6 +51,9 @@ public class Funcionario extends Pessoa {
 	}
 
 	public void setSalario(Double salario) {
+		if (salario == null || salario <= 0) {
+			throw new DomainException("Salário deve ser maior que zero");
+		}
 		this.salario = salario;
 	}
 
@@ -50,6 +62,9 @@ public class Funcionario extends Pessoa {
 	}
 
 	public void setSetor(Setor setor) {
+		if (setor == null) {
+			throw new DomainException("Setor não pode ser nulo");
+		}
 		this.setor = setor;
 	}
 
@@ -58,6 +73,9 @@ public class Funcionario extends Pessoa {
 	}
 
 	public void setFuncao(Funcao funcao) {
+		if (funcao == null) {
+			throw new DomainException("Função não pode ser nulo");
+		}
 		this.funcao = funcao;
 	}
 
