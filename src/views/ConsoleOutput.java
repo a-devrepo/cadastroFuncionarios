9cd9bd8487e963a6d3d259805ceb84c87005ea56
alison <adevpr11@gmail.com>
2025-05-19 19:42:15 -0300

    Adiciona classe ConsoleOutput

---
+++ src/views/ConsoleOutput.java
@@ -0,0 +1,12 @@
+package views;
+
+public class ConsoleOutput {
+
+	public void exibirTexto(String texto) {
+		System.out.println(texto);
+	}
+	
+	public void exibirTextoComQuebra(String texto) {
+		System.out.println("\n"+texto);
+	}
+}
